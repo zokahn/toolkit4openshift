cba1aec6e09ccdc01437d0bedbddf414fa97e0a9
Bart van den Heuvel <bart@redhat.com>
2021-08-19 08:24:25 +0200

    moved web stuff to html folder. added status page

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM registry.access.redhat.com/ubi8/ubi
 RUN yum --disableplugin=subscription-manager -y install httpd nmap iputils iproute procps-ng libxslt \
 	    && yum --disableplugin=subscription-manager clean all
 
-COPY . /var/www/html
+COPY html/. /var/www/html
 
 RUN sed -i 's/Listen 80/Listen 8080/' /etc/httpd/conf/httpd.conf \
 		    && chgrp -R 0 /var/log/httpd /var/run/httpd /var/www/html \
---
+++ bin/local-status.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+
+# Taking lots of ideas from org Author : - Shashank Srivastava https://github.com/shashank-ssriva/Linux-Server-HTML-Health-Report-Using-Shell-Script/
+# First take measurements then display them in a html
+
+
+#Declaring variables
+#set -x
+webdir="/var/www/html"
+os_name=`cat /etc/redhat-release`
+upt=`uptime | awk {'print$3'} | cut -f1 -d','`
+ip_add=`ip a show dev eth0 | grep inet | awk {'print$2'}`
+num_proc=`ps -ef | wc -l`
+root_fs_pc=`df -h / | tail -1 | awk '{print$5}'`
+total_root_size=`df -h / | tail -1 | awk '{print$2}'`
+#load_avg=`uptime | cut -f5 -d':'`
+load_avg=`cat /proc/loadavg  | awk {'print$1,$2,$3'}`
+ram_usage=`free -m | head -2 | tail -1 | awk {'print$3'}`
+ram_total=`free -m | head -2 | tail -1 | awk {'print$2'}`
+inode=`df -i / | head -2 | tail -1 | awk {'print$5'}`
+os_version=`uname -v | cut -f2 -d'~' | awk {'print$1'} | cut -f1 -d'-' | cut -c 1-5`
+#Creating a directory if it doesn't exist to store reports first, for easy maintenance.
+if [ ! -d ${webdir}/health_reports ]
+then
+  mkdir ${webdir}/health_reports
+fi
+html="${webdir}/health_reports/Server-Health-Report-`hostname`-`date +%y%m%d`-`date +%H%M`.html"
+
+for i in `ls /home`; do sudo du -sh /home/$i/* | sort -nr | grep G; done > /tmp/dir.txt
+#Generating HTML file
+echo "<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">" >> $html
+echo "<html>" >> $html
+echo "<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css">" >> $html
+echo "<body>" >> $html
+echo "<fieldset>" >> $html
+echo "<center>" >> $html
+echo "<h2>Linux Server Report" >> $html
+echo "<h3><legend>Script authored by Shashank Srivastava</legend></h3>" >> $html
+echo "</center>" >> $html
+echo "</fieldset>" >> $html
+echo "<br>" >> $html
+echo "<center>" >> $html
+echo "<h2>OS Details : </h2>" >> $html
+echo "<table class="pure-table">" >> $html
+echo "<thead>" >> $html
+echo "<tr>" >> $html
+echo "<th>OS Name</th>" >> $html
+echo "<th>OS Version</th>" >> $html
+echo "<th>IP Address</th>" >> $html
+echo "<th>Uptime</th>" >> $html
+echo "</tr>" >> $html
+echo "</thead>" >> $html
+echo "<tbody>" >> $html
+echo "<tr>" >> $html
+echo "<td>$os_name</td>" >> $html
+echo "<td>$os_version</td>" >> $html
+echo "<td>$ip_add</td>" >> $html
+echo "<td>$upt</td>" >> $html
+echo "</tr>" >> $html
+echo "</tbody>" >> $html
+echo "</table>" >> $html
+echo "<h2>Resources Utilization : </h2>" >> $html
+echo "<br>" >> $html
+echo "<table class="pure-table">" >> $html
+echo "<thead>" >> $html
+echo "<tr>" >> $html
+echo "<th># of Processes</th>" >> $html
+echo "<th>Root FS Usage</th>" >> $html
+echo "<th>Total Size of Root FS</th>" >> $html
+echo "<th>Load Average</th>" >> $html
+echo "<th>Used RAM(in MB)</th>" >> $html
+echo "<th>Total RAM(in MB)</th>" >> $html
+echo "<th>iNode Status</th>" >> $html
+echo "</tr>" >> $html
+echo "</thead>" >> $html
+echo "<tbody>" >> $html
+echo "<tr>" >> $html
+echo "<td><center>$num_proc</center></td>" >> $html
+echo "<td><center>$root_fs_pc</center></td>" >> $html
+echo "<td><center>$total_root_size</center></td>" >> $html
+echo "<td><center>$load_avg</center></td>" >> $html
+echo "<td><center>$ram_usage</center></td>" >> $html
+echo "<td><center>$ram_total</center></td>" >> $html
+echo "<td><center>$inode</center></td>" >> $html
+echo "</tr>" >> $html
+echo "</tbody>" >> $html
+echo "</table>" >> $html
+echo "<h2>Culprit Directories(Eating up disk space) : </h2>" >> $html
+echo "<br>" >> $html
+echo "<table class="pure-table">" >> $html
+echo "<thead>" >> $html
+echo "<tr>" >> $html
+echo "<th>Size</th>" >> $html
+echo "<th>Name</th>" >> $html
+echo "</tr>" >> $html
+echo "</thead>" >> $html
+echo "<tr>" >> $html
+echo "</table>" >> $html
+echo "</body>" >> $html
+echo "</html>" >> $html
+echo "Report has been generated in ${HOME}/health_reports with file-name = $html. 
